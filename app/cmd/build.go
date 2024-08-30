6edb8058d5ec6dcc69257fca78c660fb72e17427
samin <samintejas@gmail.com>
2024-08-31 02:47:23 +0530

    style changes

--- app/cmd/build.go
+++ app/cmd/build.go
@@ -11,12 +11,13 @@ const SCRIPT_HTML = `
 		<html lang="en">
 		<head>
    		<meta charset="UTF-8">
-     		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+     	<meta name="viewport" content="width=device-width, initial-scale=1.0">
        	<title>%s</title>
-        	<script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
-         	<script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
-          	<link rel="stylesheet" href="/styles/default.css">
-           <link rel="stylesheet" href="/styles/prism.css">
+        <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
+        <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
+        <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600;700&family=Source+Code+Pro&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="/styles/default.css">
+        <link rel="stylesheet" href="/styles/prism.css">
        </head>
        <body>
        %s
