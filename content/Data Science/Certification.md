95a4192a24ccdbcf7cc1619561010cf1d14d2103
samin <samintejas@gmail.com>
2024-09-04 01:47:05 +0530

    added syntax highlighting and favicon

--- content/Data Science/Certification.md
+++
@@ -1,3 +0,0 @@
-# Certification
-
-[https://www.comptia.org/certifications/data](https://www.comptia.org/certifications/data)
--- default/scripts/tree.js
+++
@@ -1,9 +0,0 @@
-const folders = document.querySelectorAll(".folder");
-
-for (const folder of folders) {
-  folder.onclick = () => {
-    folder.classList.toggle("active");
-    const target_list = folder.nextElementSibling;
-    target_list.style.maxHeight = target_list.style.maxHeight ? null : "100vh";
-  };
-}
